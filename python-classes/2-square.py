ed078e79315a365e17745a0876f743c6f63f1c16
Dominick <6157@holbertonstudents.com>
2023-05-22 20:17:18 -0700

    size validation intial push

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# write a class Square that defines a square by:(based on 1-square.py) 
+"""This is the Square class"""
+
+
+class square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """This method initializes a square with a size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
