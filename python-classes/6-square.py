435f18b48f454feae7ca8268bf5e2437587db0f0
Dominick <6157@holbertonstudents.com>
2023-05-23 13:36:45 -0700

    fixed???

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,7 +4,10 @@
 
 class Square:
     """This class defines a Square"""
-    
+   def __init__(self, size=0, position=(0,0)):
+       self.size = size
+       self.position = position
+
     @property
     def size(self):
         return self.__size
@@ -26,11 +29,6 @@ class Square:
         if type(value) is not tuple or len(value) != 2 or \ type(value[0]) is not int or type(value[1]) is not int or \ value[0] < 0 or value[1] < 0:
             raise TypeaError("position must be a tuple of 2 positive integers")
         self.__position = value
-
-    def __init__(self, size=0, position=(0, 0)):
-        """This method initializes the square with a size"""
-        self.size = size
-        self.position = position
 
     def area(self):
         return self.__size ** 2
