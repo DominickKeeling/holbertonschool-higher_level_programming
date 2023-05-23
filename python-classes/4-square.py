8f524a087483b7efc34d8705c17d35ec539b1872
Dominick <6157@holbertonstudents.com>
2023-05-22 20:58:09 -0700

    chmod all files and initial push of getter and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is the square class!"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self,value):
+        if type(value) != int:
+            raise TypeError("size must be integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Prints area"""
+        return self.__size ** 2
