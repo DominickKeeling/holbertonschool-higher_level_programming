4c656faf36c15a744474c657076d246b30d71ac5
Dominick <6157@holbertonstudents.com>
2023-05-23 18:42:00 -0700

    pep8

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # write a function with two integers. a and b must be integers or floats
-# otherwise raise a TypeError exception with the message 'a must be an integer or
-# b must be an integer
+# otherwise raise a TypeError exception with the message 'a must be an integer
+# or b must be an integer
 # a and b must be first casted to integers if they are float
 # Returns an integer: the addition of a and b
 # You are not allowed to import any module
