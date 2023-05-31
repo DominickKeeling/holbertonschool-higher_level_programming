71fdc344dfc3446db31bbf335822d13b877786c8
Dominick <6157@holbertonstudents.com>
2023-05-30 19:21:59 -0700

    initial push

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""base_geometry module"""
+
+
+class BaseGeometry():
+    """creates class called BaseGeometry"""
+
+    def area(self):
+        """area function"""
+
+        raise Exception("area() is not implemented")
