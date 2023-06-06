3e404299d85ade4e46af09739e231e38ca1ba90e
Dominick <6157@holbertonstudents.com>
2023-06-06 15:40:46 -0700

    added dictionary to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -33,3 +33,7 @@ class Square(Rectangle):
         elif kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a square"""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
