1c34f157d611403fe1dba8b6004b2bead41b7d4d
Dominick <6157@holbertonstudents.com>
2023-06-06 13:46:47 -0700

    add square module

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is the Square module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """this is the Square class object of Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id, x, y, size, size)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.y, self.size)
