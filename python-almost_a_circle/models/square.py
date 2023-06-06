eb00da19f17b3dbc2661593bf867d0d6484a5267
Dominick <6157@holbertonstudents.com>
2023-06-06 13:57:06 -0700

    return string fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,7 +6,9 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """this is the Square class object of Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id) #order matters. follow rectangle
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.y, self.width)
+        return "[Square] ({%d}) {%d}/{%d} - {%d}".format(self.id,
+                                                         self.y,
+                                                         self.width)
