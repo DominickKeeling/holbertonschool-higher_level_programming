c66a836931c1e612ff2fec6b649eb9a1500e2eb3
Dominick <6157@holbertonstudents.com>
2023-06-06 13:52:43 -0700

    super class fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """this is the Square class object of Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.y, self.width)
