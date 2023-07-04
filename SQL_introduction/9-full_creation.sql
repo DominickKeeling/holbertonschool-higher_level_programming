e9c17e0f38afd55e8be9a0a46e4c891774709477
Dominick <6157@holbertonstudents.com>
2023-07-03 20:31:26 -0700

    fixing syntax for full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- create a table second_table if it doesnt exist
-CREATE TABLE IF NOT EXISTS second_table)
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
